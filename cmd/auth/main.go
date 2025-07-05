beac09c6d049284bd91493902a844ee4c95a4239
sxd0 <fancityfun@gmail.com>
2025-07-05 17:16:32 +0500

    feat(auth): add database connection and initial users table migration

--- cmd/auth/main.go
+++ cmd/auth/main.go
@@ -6,11 +6,20 @@ import (
 
 	"github.com/sxd0/SweetTweet/pkg/config"
 	"github.com/sxd0/SweetTweet/pkg/logger"
+	"github.com/sxd0/SweetTweet/pkg/db"
 )
 
 func main() {
 	cfg := config.Load()
 	log := logger.NewLogger()
+
+	// db, err := db.Connect()
+	_, err := db.Connect()
+	if err != nil {
+		log.Error("DB connection failed", "error", err)
+		return
+	}
+	log.Info("Connected to DB")
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
@@ -18,9 +27,9 @@ func main() {
 	})
 
 	log.Info("Auth service starting", "port", cfg.Port)
-
-	err := http.ListenAndServe(fmt.Sprintf(":%s", cfg.Port), mux)
+	err = http.ListenAndServe(fmt.Sprintf(":%s", cfg.Port), mux)
 	if err != nil {
 		log.Error("Server failed to start", "error", err)
 	}
 }
+
